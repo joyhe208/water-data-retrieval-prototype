762d5237df6787d1eac14073a45e3fe52c07976e
Joy He <joyhe208@gmail.com>
2021-08-21 11:55:04 -0400

    bare bones Flask app to query water data visually

---
+++ GetData.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+import json
+import os
+from os import path
+import snowflake.connector
+import config
+from config import *
+from abc import *
+import re
+import sqltranslate
+from sqltranslate import *
+
+#######################Get Formatted Data Module#############################
+# semi-abstract parent class for querying data in different formats
+# data is saved in a specific format that can be sent to front-end Javascript systems, 
+# held in data field
+#############################################################################
+class GetFormattedData(metaclass = ABCMeta):
+    def __init__(self, filename):
+       self.filename = filename
+       #self.data = self.accessData()
+    
+    @abstractmethod
+    def accessData(self):
+        pass
+
+    def returnData(self):
+        return self.data
+
+class GetTableData(GetFormattedData):
+    def __init__(self,filename):
+        super().__init__(filename)
+
+    def accessData(self):
+        with open(self.filename,'r') as f:
+            df = pd.read_csv(f)
+        values = []
+        for i in range(len(df)):
+            dataEntry = {}
+            for column in df.columns:
+                dataEntry[column] = df.loc[i,column]
+            values.append(dataEntry)
+        return values
+    
+class GetJsonData(GetFormattedData):
+    def __init__(self,filename):
+        super().__init__(filename)
+    def accessData(self):
+        with open(self.filename,"r") as f:
+            return json.load(f)
+    
+
+class GetDatabaseTableData(GetTableData):
+    def __init__(self,filename,config, params):
+        self.query = run(params) #make sqltranslate object 
+        self.ctx = snowflake.connector.connect(
+            user= config['user'],
+            password= config['password'],
+            account= config['account'],
+            warehouse=config['warehouse'],
+            database=config['database'],
+            schema=config['schema']
+        )
+        self.cs = self.ctx.cursor()
+        self.executeCommand()
+        super().__init__(filename)
+        
+
+    def executeCommand(self):
+        records = self.cs.execute(self.query.command())
+        with open(self.filename,'w') as f:
+            records.fetch_pandas_all().to_csv(self.filename)
+
---
+++ app.py
@@ -0,0 +1,72 @@
+import flask
+from flask import Flask, Response, render_template, request, jsonify, make_response
+import json
+import GetData
+from GetData import *
+import numpy as np
+import config
+from config import *
+app = Flask(__name__)
+
+mapLayerFiles = {
+    "watersheds": "./static/data/Watersheds.json",
+    "counties": "./static/data/CACounties.json",
+    "urbanArea": "./static/data/CAUrbanArea.json",
+    "urbanAreaPopulation": "./static/data/CAUrbanAreaPopulation.csv",
+    "swp":"./static/data/SWPPath.json",
+    "services":"./static/data/ServiceAreas.json",
+    "contractors": "./static/data/Contractors.csv",
+    "stations":"./static/data/testStationSnowflake.csv",
+    "powerPlants":"./static/data/CAPowerPlants.csv" 
+}
+
+def convert(o):
+    if isinstance(o, np.int64): return int(o)  
+
+#for the map visualization
+@app.route('/',methods=["POST","GET"])
+@app.route('/index',methods=["POST","GET"])
+def index():
+    if(request.method == "POST"):
+        req = request.get_json()
+        layer = req['name']
+        if(layer == "getStreamflow"):
+            stations = req['stations']
+            dateRange = req['dateRange']
+            print(stations)
+            print(dateRange)
+            params = {
+                'categorical':False,
+                'columnList':['streamflow'],
+                'filters':
+                {
+                    'staid':{'type': 'equal',
+                            'values':stations}
+                },
+                'aggregateBy':'day',
+                'aggregate':{'streamflow':'sum'},
+                'dateRange':dateRange
+            }
+
+            getData = GetDatabaseTableData('streamflow.csv',config,params)
+            data = getData.accessData()
+            #data = GetTableData("./static/streamFlows/test11476500.csv").accessData()
+            res = Response(json.dumps(data,default=convert), mimetype='application/json')
+        else:
+            if("json" in mapLayerFiles[layer]):
+                getData = GetJsonData(mapLayerFiles[layer])
+                
+            else:
+                getData = GetTableData(mapLayerFiles[layer])
+            
+            data = getData.accessData()
+            if("csv" in mapLayerFiles[layer]):
+                res = Response(json.dumps(data,default=convert), mimetype='application/json')
+            else:
+                res = make_response(jsonify(data),200)
+        return res
+    return render_template('index.html')
+
+    
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
